ecb55bc22cb5d4da813bcb34bd05850d06672108
prajwaltr <prajwaltr.aithal@gmail.comgit clone https://github.com/ticepd/EmbSysDesign_NPTEL_Course.gitgit config --global user.email prajwaltr.aithal@gmail.com>
2022-01-03 00:31:43 +0530

    same code

--- main_code.cpp
+++ main_code.cpp
@@ -88,7 +88,8 @@ void dijkstra(int graph[9][9], int src,int destina)
 			// Update dist[v] only if is not in sptSet, there is an edge from
 			// u to v, and total weight of path from src to v through u is
 			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
+			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+               {
 				    parent[v] = u;
 				    dist[v] = dist[u] + graph[u][v];
 				}
@@ -102,15 +103,16 @@ void dijkstra(int graph[9][9], int src,int destina)
 int main()
 {   int src,dst;
     int graph[9][9] =
-      { {0, 1, 0, 9, 0, 0, 0, 0, 0},    // distance from A to all the other nodes
-        {1, 0, 3, 0, 4, 0, 0, 0, 0},    // distance from B to all the other nodes
-        {0, 3, 0, 0, 0, 2, 0, 0, 0},    // distance from C to all the other nodes
-        {9, 0, 0, 0, 8, 0, 1, 0, 0},    // distance from D to all the other nodes
-        {0, 4, 0, 8, 0, 5, 0, 1, 0},    // distance from E to all the other nodes
-        {0, 0, 2, 0, 5, 0, 0, 0, 7},    // distance from F to all the other nodes
-        {0, 0, 0, 1, 0, 0, 0, 3, 0},    // distance from G to all the other nodes
-        {0, 0, 0, 0, 1, 0, 3, 0, 4},    // distance from H to all the other nodes
-        {0, 0, 0, 0, 0, 7, 0, 4, 0} };  // distance from I to all the other nodes
+    { {0, 1, 0, 9, 0, 0, 0, 0, 0}, // distance from A to all the other nodes
+      {1, 0, 3, 0, 4, 0, 0, 0, 0}, // distance from B to all the other nodes
+      {0, 3, 0, 0, 0, 2, 0, 0, 0}, // distance from C to all the other nodes
+      {9, 0, 0, 0, 8, 0, 1, 0, 0}, // distance from D to all the other nodes
+      {0, 4, 0, 8, 0, 5, 0, 1, 0}, // distance from E to all the other nodes
+      {0, 0, 2, 0, 5, 0, 0, 0, 7}, // distance from F to all the other nodes
+      {0, 0, 0, 1, 0, 0, 0, 3, 0}, // distance from G to all the other nodes
+      {0, 0, 0, 0, 1, 0, 3, 0, 4}, // distance from H to all the other nodes
+      {0, 0, 0, 0, 0, 7, 0, 4, 0} // distance from I to all the other nodes
+     }; 
     cin>>src>>dst;
    // if(src<=dst)
 	dijkstra(graph, src, dst);     // 0 indicates node A (source node)
