4b7b2b318290fca26010a2928b517cc1ac9d1100
prajwaltr <prajwaltr.aithal@gmail.comgit clone https://github.com/ticepd/EmbSysDesign_NPTEL_Course.gitgit config --global user.email prajwaltr.aithal@gmail.com>
2022-05-03 14:05:11 +0530

    for 2nd phase

--- MainCode_withRandGraph.cpp
+++ MainCode_withRandGraph.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 using namespace std;
 #include <climits>
-#include <cstdlib>
-
 
 // function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path
 int minDistance(int dist[], bool sptSet[])
 {
 
-    // Initialize min value
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < 9; v++)
-        if (sptSet[v] == false && dist[v] <= min)
+	// Initialize min value
+	int min = INT_MAX, min_index;
+    
+	for (int v = 0; v < 9; v++)
+		if (sptSet[v] == false && dist[v] <= min)
         {
             min = dist[v], min_index = v;
         }
     //cout<<min_index<<endl;
     //cout<<"hi"<<endl;
-    return min_index;
+	return min_index;
 }
 
 void printPath(int parent[], int j)
@@ -37,77 +35,88 @@ void printPath(int parent[], int j)
 }
 
 void printSolution(int dist[], int parent[], int destina,int src)  //print the result
-{
+{  
+    /*
     cout<<"dist_array"<<endl;
-
    for(int i=0;i<9;i++)
     {
-        char anyNode = 65 + i;
-        char srcNode = 65 + src;
-        cout<<"distance from node "<<anyNode<<" to source node "<<srcNode<<" is "<<dist[i]<<endl;
+        cout<<"distance from node "<<i<<" to source node "<<src<<" is "<<dist[i]<<endl;
     }
     cout<<"parent_array"<<endl;
 
     for(int i=0;i<9;i++)
     {
-        char curNode = 65 + i;
-        char prevNode = 65 + parent[i];
-        cout<<"parent of "<<curNode<<" is "<<prevNode<<endl;
-    }
-    cout<<"\n****************\n"; 
+        cout<<"parent of "<<i<<" is "<<parent[i]<<endl;
+    }*/
+    cout<<"\n********************************************************\n";
     //cout<<dist[destina]<<" "<<parent[destina]<<endl;
-
+    
+    cout<<"Source "<<src<<" -> Detination "<<destina<<" \t Distance \t Path"<<endl;
+    
+                  
     char snode = 65+ src;
     char node = 65 + destina; //converting from number to alphabets
-
-    cout<<"\nSource "<<snode<<" -> Detination "<<node<<" \t Distance \t Path"<<endl;
-
-
-
-    cout<<"\n"<<snode<<"\t -> \t "<<node<<"\t\t  "<<dist[destina]<<"\t\t "<<snode;
-    printPath(parent, destina);
-
-
+    cout<<""<<snode<<"\t -> \t "<<node<<"\t\t  "<<dist[destina]<<"\t\t "<<snode;
+    printPath(parent, destina);     
+    
+    
     cout<<"\n";
 }
 
 void dijkstra(int graph[9][9], int src,int destina)
 {
-    int dist[9], parent[9];
+	int dist[9], parent[9];
     bool sptSet[9];
 
-    for (int i = 0; i < 9; i++)
-        dist[i] = INT_MAX, sptSet[i] = false, parent[src] = -1;
+	for (int i = 0; i < 9; i++)
+		dist[i] = INT_MAX, sptSet[i] = false, parent[src] = -1;
 
-    dist[src] = 0; //source node distnace
 
-    // Find shortest path for all vertices
-    for (int count = 0; count < 8; count++) {
-        // Pick the minimum distance vertex from the set of vertices not
-        // yet processed. u is always equal to src in the first iteration.
-        int u = minDistance(dist, sptSet);
+	dist[src] = 0; //source node distnace
 
-        // Mark the picked vertex as processed
-        sptSet[u] = true;
+	// Find shortest path for all vertices
+	for (int count = 0; count < 8; count++) {
+		// Pick the minimum distance vertex from the set of vertices not
+		// yet processed. u is always equal to src in the first iteration.
+		int u = minDistance(dist, sptSet);
 
-        // Update dist value of the adjacent vertices of the picked vertex.
-        for (int v = 0; v < 9; v++)
+		// Mark the picked vertex as processed
+		sptSet[u] = true;
 
-            // Update dist[v] only if is not in sptSet, there is an edge from
-            // u to v, and total weight of path from src to v through u is
-            // smaller than current value of dist[v]
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+		// Update dist value of the adjacent vertices of the picked vertex.
+		for (int v = 0; v < 9; v++)
+
+			// Update dist[v] only if is not in sptSet, there is an edge from
+			// u to v, and total weight of path from src to v through u is
+			// smaller than current value of dist[v]
+			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
                {
-                    parent[v] = u;
-                    dist[v] = dist[u] + graph[u][v];
-                }
+				    parent[v] = u;
+				    dist[v] = dist[u] + graph[u][v];
+				}
 
-    }
+	}
 
-    printSolution(dist, parent,destina,src);
+	printSolution(dist, parent,destina,src);
 }
 
-int randGen(int graph[9][9], int src, int dst)
+
+void printGraph(int graph[][9])
+{
+    int i,j;
+    cout<<"\n*****New Input*******\n";
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9;j++)
+        {
+            cout<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+void randGen(int graph[9][9], int src, int dst)
 {                                                         //random value generation function
 	
 	graph[0][1] = graph[1][0] = (rand() % 10) + 1;
@@ -122,10 +131,13 @@ int randGen(int graph[9][9], int src, int dst)
 	graph[5][8] = graph[8][5] = (rand() % 10) + 1;
 	graph[6][7] = graph[7][6] = (rand() % 10) + 1;
 	graph[7][8] = graph[8][7] = (rand() % 10) + 1;
-	
+   
+
+    printGraph(graph);
 	dijkstra(graph,src,dst);
 
 }
+
 
 int main()
 {   int src, dst;
